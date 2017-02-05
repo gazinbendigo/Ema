2c3161feb4983ce1313b0fa0fe23b4bf4cec518b
Gazman <garethinbendigo@gmail.com>
2017-02-05 19:28:19 +1100

    Update Roles

--- server/startup/setup.js
+++ server/startup/setup.js
@@ -38,21 +38,17 @@ Meteor.startup(function(){
 
         //Meteor requires the user must have a username field.
         var users = [
-            // {firstName: "Normal", lastName: "User", username: "adm0001", password: "12345678", email:"normal@example.com", roles: ApplicationRoles.Analyst, group: ApplicationFunctions.Analyst, userEnv: UserEnvironments.dtl},
-            // {firstName: "Installer", lastName: "User", username: "adm0002", password: "12345678", email:"installer@example.com", roles: ApplicationRoles.Installer, group: ApplicationFunctions.Installer, userEnv: UserEnvironments.dtl},
-            // {firstName: "Operator", lastName: "User", username: "adm0003", password: "12345678", email:"operator@example.com", roles: ApplicationRoles.Operator, group: ApplicationFunctions.Operator, userEnv: UserEnvironments.dtl},
-            // {firstName: "Admin", lastName: "User", username: "adm0004", password: "12345678", email:"admin@example.com", roles: ApplicationRoles.SuperUser, group: ApplicationFunctions.SuperUser, userEnv: UserEnvironments.dtl},
             {firstName: "James", lastName: "Brown", username: "adm1112", password: "12345678", email:"User1@hotmail.com", roles: ApplicationRoles.Analyst, group: ApplicationFunctions.Analyst, userEnv: UserEnvironments.dev},
             {firstName: "James", lastName: "Henry", username: "adm1122", password: "12345678", email:"aUser2@hotmail.com", roles: ApplicationRoles.Installer, group: ApplicationFunctions.Installer, userEnv: UserEnvironments.dtl},
-            {firstName: "Joe", lastName: "Brown", username: "adm1133", password: "12345678", email:"User3@hotmail.com", roles: ApplicationRoles.Operator, group: ApplicationFunctions.Operator, userEnv: UserEnvironments.dtl},
+            {firstName: "Joe", lastName: "Brown", username: "adm1133", password: "12345678", email:"User3@hotmail.com", roles: ApplicationRoles.Administrator, group: ApplicationFunctions.Administrator, userEnv: UserEnvironments.dtl},
             {firstName: "Fred", lastName: "Flintstone", username: "adm1144", password: "12345678", email:"User4@hotmail.com", roles: ApplicationRoles.SuperUser, group: ApplicationFunctions.SuperUser, userEnv: UserEnvironments.dtl},
             {firstName: "Barney", lastName: "Rubble", username: "adm1155", password: "12345678", email:"User5@hotmail.com", roles: ApplicationRoles.Analyst, group: ApplicationFunctions.Analyst, userEnv: UserEnvironments.dev},
             {firstName: "Stevie", lastName: "Wonder", username: "adm1166", password: "12345678", email:"User6@hotmail.com", roles: ApplicationRoles.Installer, group: ApplicationFunctions.Installer, userEnv: UserEnvironments.dtl},
-            {firstName: "LL", lastName: "Kool J", username: "adm1177", password: "12345678", email:"User7@hotmail.com", roles: ApplicationRoles.Operator, group: ApplicationFunctions.Operator, userEnv: UserEnvironments.dtl},
+            {firstName: "LL", lastName: "Kool J", username: "adm1177", password: "12345678", email:"User7@hotmail.com", roles: ApplicationRoles.Administrator, group: ApplicationFunctions.Administrator, userEnv: UserEnvironments.dtl},
             {firstName: "Harry", lastName: "Potter", username: "adm1188", password: "12345678", email:"User8@hotmail.com", roles: ApplicationRoles.SuperUser, group: ApplicationFunctions.SuperUser, userEnv: UserEnvironments.dtl},
             {firstName: "Elizabeth", lastName: "Turner", username: "adm1199", password: "12345678", email:"User9@hotmail.com", roles: ApplicationRoles.Analyst, group: ApplicationFunctions.Analyst, userEnv: UserEnvironments.dtl},
             {firstName: "Mario", lastName: "Wally", username: "adm1100", password: "12345678", email:"User10@hotmail.com", roles: ApplicationRoles.Installer, group: ApplicationFunctions.Installer, userEnv: UserEnvironments.dtl},
-            {firstName: "Kate", lastName: "Oslow", username: "adm0011", password: "12345678", email:"User11@hotmail.com", roles: ApplicationRoles.Operator, group: ApplicationFunctions.Operator, userEnv: UserEnvironments.dtl},
+            {firstName: "Kate", lastName: "Oslow", username: "adm0011", password: "12345678", email:"User11@hotmail.com", roles: ApplicationRoles.Administrator, group: ApplicationFunctions.Administrator, userEnv: UserEnvironments.dtl},
             {firstName: "Wheres", lastName: "Wally", username: "adm0012", password: "12345678", email:"User12@hotmail.com", roles: ApplicationRoles.SuperUser, group: ApplicationFunctions.SuperUser, userEnv: UserEnvironments.prod}
 
         ];
@@ -71,7 +67,7 @@ Meteor.startup(function(){
             const userProfile = {
                 firstName: userData.firstName,
                 lastName: userData.lastName,
-                userEnv: userData.userEnv,
+                team: userData.userEnv,
                 userGroup: (userData.group === "__global_roles__") ? "SuperUser": userData.group
             }
 
@@ -81,22 +77,39 @@ Meteor.startup(function(){
             Roles.addUsersToRoles(id, userData.roles, userData.group);
 
         });
-        let id = Meteor.users.find({"userProfile.lastName": {"$eq": 'Brown'}});
-        if(id){
-            Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
-            Roles.addUsersToRoles(id, ApplicationRoles.SuperUser, ApplicationFunctions.SuperUser);
-        }
-        id = Meteor.users.find({"userProfile.firstName": {"$eq": "Wheres"}});
-        if(id){
-            console.log(id);
-            Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
-            Roles.addUsersToRoles(id, ApplicationRoles.Analyst, ApplicationFunctions.Analyst);
-        }
-        id = Meteor.users.find({"userProfile.lastName": {"$eq": 'Rubble'}});
-        if(id){
-            Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
-            Roles.addUsersToRoles(id, ApplicationRoles.SuperUser, ApplicationFunctions.SuperUser);
-        }
+        let id = findUserId('Brown', function(err, result){
+            if(id){
+                Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
+                Roles.addUsersToRoles(id, ApplicationRoles.SuperUser, ApplicationFunctions.SuperUser);
+            }
+        });// Meteor.users.find({"userProfile.lastName": {"$eq": 'Brown'}});
+
+        id = findUserId('Wheres', function(err, result) {
+            if(id){
+                console.log(id);
+                Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
+                Roles.addUsersToRoles(id, ApplicationRoles.Analyst, ApplicationFunctions.Analyst);
+            }
+        });//Meteor.users.find({"userProfile.firstName": {"$eq": "Wheres"}});
+
+        id = findUserId('Rubble', function(err, result){
+            if(id){
+                // updateUser(id, 'userProfile.userGroup', "SuperUser", function(err, result){
+                //
+                // });
+                Meteor.users.update({_id:id}, {$set:{'userProfile.userGroup': "SuperUser"}});
+                Roles.addUsersToRoles(id, ApplicationRoles.SuperUser, ApplicationFunctions.SuperUser);
+            }
+        });//Meteor.users.find({"userProfile.lastName": {"$eq": 'Rubble'}});
     }
 
 });
+
+function findUserId(lastName, cb){
+    let id = Meteor.users.find({"userProfile.lastName": {"$eq": lastName}});
+    cb(null, id);
+};
+
+function updateUser(id, col, value, cb){
+    Meteor.users.update({_id:id}, {$set:{col: value}});
+}
