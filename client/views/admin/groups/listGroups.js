e88232009c5f2da0d8c40fbeab1f6928cc252150
Gazzman <garethinbendigo@gmail.com>
2017-05-09 20:25:00 +1000

    Update ListGroups styling layout and events

--- client/views/admin/groups/listGroups.js
+++ client/views/admin/groups/listGroups.js
@@ -4,6 +4,7 @@
 
 Template.listGroups.onCreated(function() {
     Meteor.subscribe("groups");
+    this.groupItem = new ReactiveVar(null);
 });
 
 Template.listGroups.helpers({
@@ -14,9 +15,23 @@ Template.listGroups.helpers({
     groupUpadtePath(groupId) {
         let param = {id: groupId};
         return FlowRouter.path("manageGroup", param);
+    },
+
+    setGroupItem(id){
+        console.log(id);
+      Template.instance().groupItem.set(id);
     }
 });
 
 Template.listGroups.events({
+    'click .groupItem': function(evt, template){
+        evt.preventDefault();
+        console.log(this.groupId);
+        console.log(this._id);
+    },
 
+    'click .groupItemDelete': function(evt, template){
+        evt.preventDefault();
+        console.log(this.groupId);
+    }
 });
